aa02d8abcbc7afcbf3e629d7d43592369a478cb3
akshayjagtap1111 <93571686+akshayjagtap1111@users.noreply.github.com>
2022-10-26 09:36:20 +0530

    find video route

--- src/controllers/video_controller.js
+++ src/controllers/video_controller.js
@@ -15,22 +15,20 @@ router.get("/all", async (req, res) => {
 });
 
 router.get("/for_user/:userID", async (req, res) => {
-    try {
-        let userID = req.params.userID;
-      const videos = await Video.find({}).lean().exec();
-      res.status(200).send(videos);
-    } catch (err) {
-      res.status(500).json({
-        message: err.message,
-        success: false,
-      });
-    }
-  });
+  try {
+    let userID = req.params.userID;
+    const videos = await Video.find({ owner: userID }).lean().exec();
+    res.status(200).send(videos);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      success: false,
+    });
+  }
+});
 
 router.post("/add", async (req, res) => {
   try {
-
-
   } catch (err) {
     res.status(500).json({
       message: err.message,
